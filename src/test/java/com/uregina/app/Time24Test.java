3253901f28613afc4f2f26841bda988c9717a33e
Jonathan Vargas <jonathanbvargas@hotmail.com>
2021-04-27 15:01:38 -0600

    Created test file for time 24

---
+++ src/test/java/com/uregina/app/Time24Test.java
@@ -0,0 +1,27 @@
+package com.uregina.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.uregina.exceptions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class Time24Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()throws InvalidTimeException
+    {   
+        
+        Time12 tester = new Time12(1, 30, AmPm.pm);
+        
+        AmPm yeeters = tester.getAM_or_PM();
+        boolean testing = ((tester.getAM_or_PM()) == AmPm.pm);
+        assertTrue( testing );
+    }
+}
